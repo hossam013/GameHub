efa2d89cd708cb100e35e7774c770e9d94eb9bf5
hossam013 <hossamhassan013@gmail.com>
2023-08-22 21:31:07 +0300

    Filtering  by genre

--- GameHub/src/components/GenreList.tsx
+++ GameHub/src/components/GenreList.tsx
@@ -1,10 +1,14 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
-import useGenres from "../hooks/useGenres";
+import { Button, HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../utils/optimizeImage";
 import GenreSkeleton from "./GenreSkeleton";
 import GenreCardContainer from "./GenreCardContainer";
 
-const GenreList = () => {
+interface Props {
+  onSelectGenre: (selectedGenre: Genre) => void;
+}
+
+const GenreList = ({ onSelectGenre }: Props) => {
   const { data: genres, error, isLoading } = useGenres();
 
   const skeletons = Array.from(Array(15).keys());
@@ -14,20 +18,26 @@ const GenreList = () => {
       {error && <Text>{error}</Text>}
       <List>
         {isLoading &&
-          skeletons.map((s) => (
-            <GenreCardContainer key={s}>
+          skeletons.map((skeleton) => (
+            <GenreCardContainer key={skeleton}>
               <GenreSkeleton />
             </GenreCardContainer>
           ))}
-        {genres.map((g) => (
-          <ListItem key={g.id} paddingY="5px">
+        {genres.map((genre) => (
+          <ListItem key={genre.id} paddingY="5px">
             <HStack>
               <Image
-                src={getCroppedImageUrl(g.image_background)}
+                src={getCroppedImageUrl(genre.image_background)}
                 boxSize="32px"
                 borderRadius={8}
               />
-              <Text fontSize="lg">{g.name}</Text>
+              <Button
+                fontSize="lg"
+                variant="link"
+                onClick={() => onSelectGenre(genre)}
+              >
+                {genre.name}
+              </Button>
             </HStack>
           </ListItem>
         ))}
--- GameHub/src/hooks/useData.ts
+++ GameHub/src/hooks/useData.ts
@@ -1,36 +1,46 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import { AxiosRequestConfig, CanceledError } from "axios";
 
 interface FetchResponse<T> {
   count: number;
   results: T[];
 }
 
-const useData = <T>(endPoint: string) => {
+const useData = <T>(
+  endPoint: string,
+  requestConfig?: AxiosRequestConfig,
+  deps?: any[]
+) => {
   const [data, setData] = useState<T[]>([]);
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    const controller = new AbortController();
+  useEffect(
+    () => {
+      const controller = new AbortController();
 
-    setIsLoading(true);
+      setIsLoading(true);
 
-    apiClient
-      .get<FetchResponse<T>>(endPoint, { signal: controller.signal })
-      .then((res) => {
-        setData(res.data.results);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        if (error instanceof CanceledError) return;
-        setError(error.message);
-        setIsLoading(false);
-      });
+      apiClient
+        .get<FetchResponse<T>>(endPoint, {
+          signal: controller.signal,
+          ...requestConfig,
+        })
+        .then((res) => {
+          setData(res.data.results);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          if (error instanceof CanceledError) return;
+          setError(error.message);
+          setIsLoading(false);
+        });
 
-    return () => controller.abort();
-  }, []);
+      return () => controller.abort();
+    },
+    deps ? [...deps] : []
+  );
 
   return { data, error, isLoading };
 };
