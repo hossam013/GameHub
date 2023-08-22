d9c6f8445696c019a0de4a1b7f965deee48452e7
hossam013 <hossamhassan013@gmail.com>
2023-08-22 22:28:03 +0300

    Highlighting the selected genre

--- GameHub/src/App.tsx
+++ GameHub/src/App.tsx
@@ -1,4 +1,3 @@
-// import { useState } from "react";
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
@@ -18,7 +17,7 @@ function App() {
         }}
         templateColumns={{
           base: "1fr",
-          lg: "200px 1fr",
+          lg: "230px 1fr",
         }}
       >
         <GridItem area="nav">
@@ -28,6 +27,7 @@ function App() {
           <GridItem area="aside" paddingX={5}>
             <GenreList
               onSelectGenre={(selectedGenre) => setSelectedGenre(selectedGenre)}
+              selectedGenre={selectedGenre}
             />
           </GridItem>
         </Show>
--- GameHub/src/components/GenreList.tsx
+++ GameHub/src/components/GenreList.tsx
@@ -1,17 +1,29 @@
-import { Button, HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import {
+  Button,
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Text,
+  useColorMode,
+} from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../utils/optimizeImage";
 import GenreSkeleton from "./GenreSkeleton";
 import GenreCardContainer from "./GenreCardContainer";
+import theme from "../Theme";
 
 interface Props {
   onSelectGenre: (selectedGenre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   const { data: genres, error, isLoading } = useGenres();
 
   const skeletons = Array.from(Array(15).keys());
+
+  const { colorMode } = useColorMode();
 
   return (
     <>
@@ -32,11 +44,21 @@ const GenreList = ({ onSelectGenre }: Props) => {
                 borderRadius={8}
               />
               <Button
-                fontSize="lg"
+                fontSize="md"
                 variant="link"
                 onClick={() => onSelectGenre(genre)}
+                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+                color={colorMode === "light" ? "blackAlpha.900" : ""}
               >
-                {genre.name}
+                <Text
+                  color={theme.config.initialColorMode === "dark" ? "" : ""}
+                >
+                  {genre.name.length < 12
+                    ? genre.name
+                    : genre.id !== selectedGenre?.id
+                    ? genre.name.slice(0, 12) + ".."
+                    : genre.name}
+                </Text>
               </Button>
             </HStack>
           </ListItem>
