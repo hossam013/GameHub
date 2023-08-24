84bbd9ddb53ce41c5f6c28f9a20476f095f1554e
hossam013 <hossamhassan013@gmail.com>
2023-08-24 03:55:44 +0300

    Handle games with no image

--- GameHub/src/utils/optimizeImage.ts
+++ GameHub/src/utils/optimizeImage.ts
@@ -1,8 +1,12 @@
+import NoImage from "../assets/noImageGame.png";
+
 const getCroppedImageUrl = (url: string) => {
+  if (!url) return NoImage;
+
   const target = "media/";
 
-  const index = url?.indexOf(target) + target.length;
-  return url?.slice(0, index) + "crop/600/400/" + url?.slice(index);
+  const index = url.indexOf(target) + target.length;
+  return url.slice(0, index) + "crop/600/400/" + url.slice(index);
 };
 
 export default getCroppedImageUrl;
